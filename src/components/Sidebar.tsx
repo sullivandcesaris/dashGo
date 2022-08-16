0908894205aae03a4ef416175ec2b0beb34465c8
sullivandcesaris <sullivandcesaris.dev@gmail.com>
2022-08-16 20:01:17 -0300

    APP: users

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -8,29 +8,29 @@ export function Sidebar() {
                 <Box>
                     <Text fontWeight="bold" color="gray.400" fontSize="small">GERAL</Text>
                     <Stack spacing="4" mt="8" align="stretch">
-                        <Link display="flex" align="center">
+                        <Link display="flex">
                             <Icon as={RiDashboardLine} fontSize="20" />
                             <Text ml="4" fontWeight="medium">Dashboard</Text>
                         </Link>
-                        <Link display="flex" align="center">
+                        <Link display="flex">
                             <Icon as={RiContactsLine} fontSize="20" />
                             <Text ml="4" fontWeight="medium">Usuários</Text>
                         </Link>
                     </Stack>
-                </Box> 
+                </Box>
                 <Box>
                     <Text fontWeight="bold" color="gray.400" fontSize="small">AUTOMAÇÃO</Text>
                     <Stack spacing="4" mt="8" align="stretch">
-                        <Link display="flex" align="center">
+                        <Link display="flex">
                             <Icon as={RiInputMethodLine} fontSize="20" />
                             <Text ml="4" fontWeight="medium">Formulários</Text>
                         </Link>
-                        <Link display="flex" align="center">
+                        <Link display="flex">
                             <Icon as={RiGitMergeLine} fontSize="20" />
                             <Text ml="4" fontWeight="medium">Automação</Text>
                         </Link>
                     </Stack>
-                </Box> 
+                </Box>
             </Stack>
         </Box>
     );
